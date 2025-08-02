69e2d4dc6a4afef1f9727104130cbcf8d090169a
niteboy17 <akbartausif@gmail.com>
2025-08-02 14:25:15 +0600

    1-project

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import connectMongoDB from './db/conncectMongoDB.js';
+import connectMongoDB from './db/connectMongoDB.js';
 import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 
 app.use("/api/auth", authRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
   connectMongoDB();
 });
